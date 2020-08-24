85112a248ce5b2d611214903dee3d7e563e9661a
Jon Pontet <jon@jonpontet.com>
2020-08-24 20:43:11 +0200

    demo update

--- js/demo.js
+++ js/demo.js
@@ -1,19 +1,5 @@
-(function($) {
-	function init() {
-
-		var options = {
-			type: 'standard',
-			active_position: 70,
-			strip_size : 'full',
-			fixed: true,
-			stackActivationWidth: 900,
-			showNextSlideButton: true,
-			nextSlideButtonSelector: ".next-slide-btn",
-			
-		};
-
-		$('section').stacked_strips(options);
-	}
-  $(init);
-})(jQuery);
+/* global $ */
+$(document).ready(function () {
+  $('section').stacked_strips();
+});
 
